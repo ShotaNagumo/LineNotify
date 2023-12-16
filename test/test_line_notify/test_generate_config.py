8b680b7ecce0c5cfd951a3664d0cf7c39ad0ca2e
Shota NAGUMO <snagumo.maron+github@gmail.com>
2023-12-17 01:57:48 +0900

    テスト追加（test_generate_config）

--- test/test_line_notify/test_generate_config.py
+++ test/test_line_notify/test_generate_config.py
@@ -9,16 +9,23 @@ def test_create_config_file():
     project_root = project_root.resolve()
     config_dir = project_root / 'config'
     variable_dir = project_root / 'test' / 'test_ws'
+    test_dir = project_root / 'test'
+    test_input_dir = test_dir / 'test_resource' / 'input'
+    test_expect_dir = test_dir / 'test_resource' / 'expect'
     try:
         # インスタンス作成
         variable_dir.mkdir()
         instance = GenerateConfig(project_root)
 
-        # 設定ファイル作成1
+        # 設定ファイル作成確認1
+        with open(Path(test_expect_dir / 'config.1.yaml'), mode='rt', encoding='utf-8') as fp:
+            expect_data = fp.read()
         test_data = {'config_version': '1', 'variable_dir': variable_dir}
         instance.create_config_file(**test_data)
-
-        assert True
+        with open(Path(config_dir / 'config.yaml'), mode='rt', encoding='utf-8') as fp:
+            generated_data = fp.read()
+        assert expect_data == generated_data
+        print(expect_data)
 
     except Exception:
         pass
