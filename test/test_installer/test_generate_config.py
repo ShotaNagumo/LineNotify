9e3e0da0ee96e40c474f389fd076b21e0a6775a9
Shota NAGUMO <snagumo.maron+github@gmail.com>
2023-12-17 02:36:46 +0900

    入力値バリデーション処理＆テストを作成

--- test/test_installer/test_generate_config.py
+++ test/test_installer/test_generate_config.py
@@ -1,3 +1,5 @@
+import pytest
+import re
 from pathlib import Path
 import shutil
 
@@ -10,7 +12,6 @@ def test_create_config_file():
     config_dir = project_root / 'config'
     variable_dir = project_root / 'test' / 'test_ws'
     test_dir = project_root / 'test'
-    test_input_dir = test_dir / 'test_resource' / 'input'
     test_expect_dir = test_dir / 'test_resource' / 'expect'
     try:
         # インスタンス作成
@@ -27,8 +28,16 @@ def test_create_config_file():
         assert expect_data == generated_data
         print(expect_data)
 
-    except Exception:
-        pass
+        # 設定ファイル作成確認2（config_versionが不正）
+        test_data = {'config_version': '', 'variable_dir': variable_dir}
+        with pytest.raises(Exception, match=re.escape('Invalid argument. (config_version)')):
+            instance.create_config_file(**test_data)
+
+        # 設定ファイル作成確認3（variable_dirが不正）
+        test_data = {'config_version': '100', 'variable_dir': ''}
+        with pytest.raises(Exception, match=re.escape('Invalid argument. (variable_dir)')):
+            instance.create_config_file(**test_data)
+
     finally:
         shutil.rmtree(variable_dir)
         shutil.rmtree(config_dir)
