8104ed1c1f62586bfeab2d384498ce63e732ffc5
Shota NAGUMO <snagumo.maron+github@gmail.com>
2023-12-21 01:20:31 +0900

    _notify_to_line() 関数のエラーハンドリング部分のテスト作成

--- test/test_line_notify/test_nagaoka_main.py
+++ test/test_line_notify/test_nagaoka_main.py
@@ -7,7 +7,7 @@ import requests
 
 from installer.generate_config import GenerateConfig
 from line_notify.nagaoka_main import NagaokaMain
-from line_notify.errors import DownloadPageError
+from line_notify.errors import DownloadPageError, NotifyError
 
 
 project_root = Path(__file__) / '..' / '..' / '..'
@@ -31,17 +31,17 @@ def test_download_page(mocker):
 
         # ConnectionError
         with mocker.patch('requests.get', side_effect=requests.ConnectionError()):
-            with pytest.raises(DownloadPageError):
+            with pytest.raises(DownloadPageError, match=re.escape('Faild to download webpage. (ConnectionError)')):
                 instance._download_page()
 
         # Timeout
         with mocker.patch('requests.get', side_effect=requests.Timeout()):
-            with pytest.raises(DownloadPageError):
+            with pytest.raises(DownloadPageError, match=re.escape('Faild to download webpage. (Timeout)')):
                 instance._download_page()
 
         # RequestsException
         with mocker.patch('requests.get', side_effect=requests.RequestException()):
-            with pytest.raises(DownloadPageError):
+            with pytest.raises(DownloadPageError, match=re.escape('Faild to download webpage. (RequestException)')):
                 instance._download_page()
 
         # HTTPError
@@ -50,6 +50,48 @@ def test_download_page(mocker):
         with mocker.patch('requests.get', return_value=res):
             with pytest.raises(DownloadPageError):
                 instance._download_page()
+
+    finally:
+        # loggerを停止（この処理を行わないと、次のディレクトリ削除処理でエラーとなりテスト失敗する）
+        logging.shutdown()
+
+        if variable_dir.exists():
+            shutil.rmtree(variable_dir)
+        if config_dir.exists():
+            shutil.rmtree(config_dir)
+
+
+def test_notify_to_line(mocker):
+    try:
+        # テスト用設定ファイル作成
+        config_data = {'config_version': '1', 'variable_dir': variable_dir}
+        gen_conf = GenerateConfig(project_root)
+        gen_conf.create_config_file(**config_data)
+
+        # インスタンス生成
+        instance = NagaokaMain(project_root)
+
+        # ConnectionError
+        with mocker.patch('requests.post', side_effect=requests.ConnectionError()):
+            with pytest.raises(NotifyError, match=re.escape('Faild to post message. (ConnectionError)')):
+                instance._notify_to_line('dummy_token', 'dummy_msg')
+
+        # Timeout
+        with mocker.patch('requests.post', side_effect=requests.Timeout()):
+            with pytest.raises(NotifyError, match=re.escape('Faild to post message. (Timeout)')):
+                instance._notify_to_line('dummy_token', 'dummy_msg')
+
+        # RequestsException
+        with mocker.patch('requests.post', side_effect=requests.RequestException()):
+            with pytest.raises(NotifyError, match=re.escape('Faild to post message. (RequestException)')):
+                instance._notify_to_line('dummy_token', 'dummy_msg')
+
+        # HTTPError
+        res = requests.Response()
+        res.status_code = 400
+        with mocker.patch('requests.post', return_value=res):
+            with pytest.raises(NotifyError):
+                instance._notify_to_line('dummy_token', 'dummy_msg')
 
     finally:
         # loggerを停止（この処理を行わないと、次のディレクトリ削除処理でエラーとなりテスト失敗する）
