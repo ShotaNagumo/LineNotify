34cf0fdbe19612575022be3929289ea529eddc6b
Shota NAGUMO <snagumo.maron+github@gmail.com>
2024-08-06 00:30:47 +0900

    logger_initializer 用テストコード作成

--- line_notify/tests/test_logger_initializer.py
+++ line_notify/tests/test_logger_initializer.py
@@ -1,36 +1,81 @@
 from pathlib import Path
 import logging
 import pytest
+import shutil
 
-from line_notify.src.logger_initializer import initialize_logger
+from line_notify.src.logger_initializer import initialize_logger, initialize_logger_auto
+
 
 template_dir = Path(__file__).parent.parent / 'resource' / 'templates'
+test_dir = Path(__file__).parent / '.testdir'
 
 
-@pytest.fixture
-def create_log_dir(tmpdir):
-    log_dir: Path = Path(tmpdir)
-    yield log_dir
-
-
-def test_initialize_logger_1(create_log_dir):
+def test_initialize_logger_1():
     try:
+        # テストディレクトリ作成
+        test_dir.mkdir(exist_ok=True)
+
         # logger初期化確認1（成功）
-        ret = initialize_logger(template_dir, create_log_dir, 'logger_test')
+        ret = initialize_logger(template_dir, test_dir)
         assert ret is None
 
     finally:
         # loggerを停止（この処理を行わないと、次のディレクトリ削除処理でエラーとなりテスト失敗する）
         logging.shutdown()
 
+        # テストディレクトリ削除
+        shutil.rmtree(test_dir)
 
-def test_initialize_logger_2(mocker, create_log_dir):
+
+def test_initialize_logger_2(mocker):
     try:
+        # テストディレクトリ作成
+        test_dir.mkdir(exist_ok=True)
+
         # logger初期化確認2（失敗）
         with (mocker.patch('pathlib.Path.mkdir', side_effect=Exception()),
              pytest.raises(Exception)):
-            initialize_logger(template_dir, create_log_dir, 'logger_test')
+            initialize_logger(template_dir, test_dir)
 
     finally:
         # loggerを停止
         logging.shutdown()
+
+        # テストディレクトリ削除
+        shutil.rmtree(test_dir)
+
+
+def test_initialize_logger_auto_1(mocker):
+    try:
+        # テストディレクトリ作成
+        test_dir.mkdir(exist_ok=True)
+
+        # logger初期化確認1（成功）
+        with mocker.patch('line_notify.src.ln_config.LnConfig.getLnVariableDir', return_value=test_dir):
+            ret = initialize_logger_auto()
+            assert ret is None
+
+    finally:
+        # loggerを停止（この処理を行わないと、次のディレクトリ削除処理でエラーとなりテスト失敗する）
+        logging.shutdown()
+
+        # テストディレクトリ削除
+        shutil.rmtree(test_dir)
+
+
+def test_initialize_logger_auto_2(mocker):
+    try:
+        # テストディレクトリ作成
+        test_dir.mkdir(exist_ok=True)
+
+        # logger初期化確認2（失敗）
+        with (mocker.patch('line_notify.src.ln_config.LnConfig.getLnVariableDir', return_value=None),
+              pytest.raises(Exception)):
+            initialize_logger_auto()
+
+    finally:
+        # loggerを停止
+        logging.shutdown()
+
+        # テストディレクトリ削除
+        shutil.rmtree(test_dir)
